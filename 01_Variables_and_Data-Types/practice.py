f59e4810007b0d5273f5cede3a130475d32bb5e1
M.Haris Ahmad <muhammadharisahmad82@gmail.com>
2026-02-11 19:10:01 +0500

    Added task 2: Rules of variables

--- 01_Variables_and_Data-Types/practice.py
+++ 01_Variables_and_Data-Types/practice.py
@@ -1,5 +1,5 @@
 #Topic: Variables and Data-Types
-#------------------------------------------------------
+#-----------------------------------
 #Task 1: Personal info variables
 
 name = "M.Haris Ahmad"                 #string
@@ -13,3 +13,19 @@ print("Name: ",name)
 print("Age: ",age)
 print("Height(meters): ",height_m)
 print("Is student: ",is_student)
+
+#____________________________________________________________________
+#____________________________________________________________________
+
+# Task 2: Variable naming rules
+#-----------------------------------
+
+# 2number = 3            Error: Variable name cannot start with a number!
+# twenty$ = 20           Error: No special character is allowed
+# Allan Smith = True     Error: No whitespace allowed
+
+# These are correct variable names:
+
+Cat_name = "Billy"       # Obeys rules of variables
+cat_age_months = 2       # Obeys rules of variables
+cat_status = "Fine"      # Obeys rules of variables
---
+++ 01_Variables_and_Data-Types/testing.py
@@ -0,0 +1,12 @@
+# Task 2: Variable naming rules
+#-----------------------------------
+
+# 2number = 3            Error: Variable name cannot start with a number!
+# twenty$ = 20           Error: No special character is allowed
+# Allan Smith = True     Error: No whitespace allowed
+
+# These are correct variable names:
+
+Cat_name = "Billy"       # Obeys rules of variables
+cat_age_months = 2       # Obeys rules of variables
+cat_status = "Fine"      # Obeys rules of variables
