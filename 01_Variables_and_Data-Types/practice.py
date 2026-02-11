f6f4c172212b86b81a18cf906650e8cd107e7be2
M.Haris Ahmad <muhammadharisahmad82@gmail.com>
2026-02-11 18:55:14 +0500

    Added task 1: Personal info variables

---
+++ 01_Variables_and_Data-Types/practice.py
@@ -0,0 +1,15 @@
+#Topic: Variables and Data-Types
+#------------------------------------------------------
+#Task 1: Personal info variables
+
+name = "M.Haris Ahmad"                 #string
+age = 17                               #integer
+height_m = 1.72                        #float
+is_student = True                      #boolean
+
+#print all the information
+
+print("Name: ",name)
+print("Age: ",age)
+print("Height(meters): ",height_m)
+print("Is student: ",is_student)
