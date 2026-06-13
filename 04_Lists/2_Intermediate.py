755981a743d32c4b15d453865644709a47a99d6b
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-13 10:56:04 +0500

    Added Intermediate list file for some deep learning

---
+++ 04_Lists/2_Intermediate.py
@@ -0,0 +1,8 @@
+scores = [[10, 15, 12], [20, 18, 21], [15, 12, 16]]
+
+first_player = scores[0][0] + scores[0][1] + scores[0][2]
+second_player = scores[1][0] + scores[1][1] + scores[1][2]
+third_player = scores[2][0] + scores[2][1] + scores[2][2]
+total_scores = [first_player, second_player, third_player]
+
+print(total_scores)
