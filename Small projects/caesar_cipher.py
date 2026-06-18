ee0e57aeacc71c1665147c62d2306ac0e418927a
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-18 22:34:16 +0500

    feat: Added a function caesar and put all the code inside it + deleted text n shift var's

--- Small projects/caesar_cipher.py
+++ Small projects/caesar_cipher.py
@@ -1,7 +1,7 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-translation_table = str.maketrans(alphabet, shifted_alphabet)
-text = 'hello world'
-encrypted_text = text.translate(translation_table)
-print(encrypted_text)
+def caesar(text, shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
+
