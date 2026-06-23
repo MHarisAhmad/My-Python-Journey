13daa7aae96a1369af645ded79dc9b15a423f77a
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-23 11:11:33 +0500

    feat: Added planet_class python file based on classes and objects

---
+++ Small projects/planet_class.py
@@ -0,0 +1,27 @@
+class Planet:
+    def __init__(self, name, planet_type, star):
+        if not (isinstance(name, str) and isinstance(planet_type, str) and isinstance(star, str)):
+            raise TypeError("name, planet type, and star must be strings")
+        
+        if (name == "") or (planet_type == "") or (star == ""):
+            raise ValueError("name, planet_type, and star must be non-empty strings")
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+    def orbit(self):
+        return f"{self.name} is orbiting around {self.star}..."
+    
+    def __str__(self):
+        return f"Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}"
+
+planet_1 = Planet("earth", "normal", "sun")
+planet_2 = Planet("earth", "normal", "sun")
+planet_3 = Planet("earth", "normal", "sun")
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
