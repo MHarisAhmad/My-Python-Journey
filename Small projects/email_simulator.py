d0bb043ff618d974518433864ab8d3123efe2e7d
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-25 09:51:42 +0500

    feat: Added email deletion method and logic

--- Small projects/email_simulator.py
+++ Small projects/email_simulator.py
@@ -42,14 +42,26 @@ class Inbox:
         if not self.emails:
             print('Your inbox is empty.\n')
             return
-        print("\nYour Emails:")
-        for i, email in enumerate(self.emails, start = 1):
-            print(f"{i}. {email}")
+        print('\nYour Emails:')
+        for i, email in enumerate(self.emails, start=1):
+            print(f'{i}. {email}')
+
     def read_email(self, index):
         if not self.emails:
-            print("Inbox is empty.\n")
+            print('Inbox is empty.\n')
             return
         actual_index = index - 1
         if actual_index < 0 or actual_index >= len(self.emails):
-            print("Invalid email number.\n")
+            print('Invalid email number.\n')
             return
+        self.emails[actual_index].display_full_email()
+    def delete_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        del(self.emails[actual_index])
+        print("Email deleted.\n")
