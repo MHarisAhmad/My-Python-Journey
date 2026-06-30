1c84a591eb8f525d1a12fda5ffafe03b7d77b9d7
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-06-30 17:50:55 +0500

    feat: Added a class attribute + Type checker if statement + __repr__ method

--- Small projects/salary_tracker.py
+++ Small projects/salary_tracker.py
@@ -1,10 +1,15 @@
 class Employee:
     def __init__(self, name, level):
+        if not isinstance(name, str) or not isinstance(level, str):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
         self._name = name
         self._level = level
 
     def __str__(self):
         return f'{self.name}: {self.level}'
+    
+    def __repr__(self):
+        return f"Employee('{self._name}', '{self._level}')"
 
     @property
     def name(self):
