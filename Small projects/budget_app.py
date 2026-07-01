df3c0f357cf0b05b86b211932885cc7af74c46d6
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-07-01 12:38:51 +0500

    feat: Added transfer method and updated display using __str__ method

--- Small projects/budget_app.py
+++ Small projects/budget_app.py
@@ -16,6 +16,14 @@ class Category:
         else:
             return False
         
+    def transfer(self, amount, destination):
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {destination.name}")
+            destination.deposit(amount, f"Transfer from {self.name}")
+            return True
+        else:
+            return False
+        
         
     
     def get_balance(self):
@@ -25,15 +33,38 @@ class Category:
         return total
     
     def check_funds(self, amount):
-        if amount < self.get_balance():
+        if amount <= self.get_balance():
             return True
         else:
             return False
+    
+    def __str__(self):
+        output = ""
+
+    # Title
+        output += self.name.center(30, "*") + "\n"
+
+    # Transactions
+        for item in self.ledger:
+            description = item["description"][:23]
+            amount = f"{item['amount']:.2f}"
+
+            output += f"{description:<23}{amount:>7}\n"
+
+    # Total
+        output += f"Total: {self.get_balance():.2f}"
+
+        return output
     
 def create_spend_chart(categories):
     pass
 
 person = Category("Ali")
-person.deposit(500, "First deposit")
-print(person.withdraw(600,"first withdraw"))
-print(person.ledger)
+second = Category("Ahmad")
+person.deposit(500, "First")
+person.withdraw(200, "First withdraw")
+person.transfer(200, second)
+print(person)
+
+second.transfer(100, person)
+print(second)
