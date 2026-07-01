042062a7a6a55cf641ac3cdd43522ca6b43ae85e
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-07-01 11:47:01 +0500

    feat: Added check_funds method which is linked with get_balance method for validity

--- Small projects/budget_app.py
+++ Small projects/budget_app.py
@@ -8,6 +8,13 @@ class Category:
             'description': description
         })
         return f"Added {amount} in the ledger"
+    def withdraw(self, amount, description = ""):
+        if self.check_funds(amount):
+            return "Valid amount"
+        else:
+            return "Invalid amount"
+        
+        
     
     def get_balance(self):
         total = 0
@@ -15,7 +22,15 @@ class Category:
             total += item["amount"]
         return total
     
+    def check_funds(self, amount):
+        if amount < self.get_balance():
+            return True
+        else:
+            return False
+    
 def create_spend_chart(categories):
     pass
 
-
+person = Category("Ali")
+person.deposit(500, "init")
+print(person.withdraw(501,"my first withdraw check"))
