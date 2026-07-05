4096b90a542929b3a411f55b79eac5c1b87a8a11
MHarisAhmad <muhammadharisahmad82@gmail.com>
2026-07-05 12:35:05 +0500

    feat: Added salary.py file's logic

--- Small projects/salary_tracker.py
+++ Small projects/salary_tracker.py
@@ -9,8 +9,11 @@ class Employee:
     def __init__(self, name, level):
         if not isinstance(name, str) or not isinstance(level, str):
             raise TypeError("'name' and 'level' attribute must be of type 'str'.")
+        if not level in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
         self._name = name
         self._level = level
+        self._salary = Employee._base_salaries[level]
 
     def __str__(self):
         return f'{self.name}: {self.level}'
